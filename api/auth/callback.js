7bc7607450654691419917de785a5e86bd52f71f
Votizo <bhandaribasudev68@gmail.com>
2025-03-23 13:25:02 +0545

    Update callback.js

--- api/auth/callback.js
+++ api/auth/callback.js
@@ -1,9 +1,12 @@
 const passport = require('../../lib/passport');
 
 module.exports = (req, res) => {
-  passport.authenticate('google', {
-    failureRedirect: '/',
-  })(req, res, () => {
+  passport.authenticate('google', { session: false }, (err, user) => {
+    if (err || !user) {
+      return res.status(500).json({ error: 'Authentication failed' });
+    }
+    // Set a simple cookie instead of session
+    res.cookie('user_id', user.id, { httpOnly: true, secure: true });
     res.redirect('https://votizo-frontend.vercel.app/');
-  });
+  })(req, res);
 };
